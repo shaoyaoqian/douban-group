e97032c27528b6b9827f5c163be755705669fe5e
Ma Pengfei <115222128+shaoyaoqian@users.noreply.github.com>
2023-01-28 14:02:56 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -37,12 +37,12 @@ for i in r.smembers(redis_douban_group_all_topics):
     print(topic_title,topic_id,topic_url,topic_content)
     topic_comments = []
     for comment in r.smembers(redis_douban_group_topic_comments.format(topicid=topic_id)):
-        topic_comments.append((b'' or comment).decode('utf-8'))
+        topic_comments.append((comment or b'').decode('utf-8'))
     topic = {}
     topic['id'] = topic_id
-    topic['content'] = (b'' or topic_content).decode('utf-8')
-    topic['title']= (b'' or topic_title).decode('utf-8')
-    topic['url'] =  (b'' or topic_url).decode('utf-8')
+    topic['content'] = (topic_content).decode('utf-8')
+    topic['title']= (topic_title or b'').decode('utf-8')
+    topic['url'] =  (topic_url or b'').decode('utf-8')
     topic['comments'] = topic_comments
     data.append(topic)
 
